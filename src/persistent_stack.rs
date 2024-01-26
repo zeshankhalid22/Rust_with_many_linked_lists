313531a9d90ee0d04ab40b68c7a8b61eb886a132
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2024-01-26 10:16:21 +0500

    created basic layout using RC smart pointer

--- src/persistent_stack.rs
+++ src/persistent_stack.rs
@@ -1,10 +1,29 @@
+use std::rc::Rc;
+
 pub struct List<T> {
     head: Link<T>,
 }
 
-type Link<T> = Option<Box<Node<T>>>;
+type Link<T> = Option<Rc<Node<T>>>;
 
 struct Node<T> {
     elem: T,
     next: Link<T>,
 }
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {head: None}
+    }
+
+    // push
+    pub fn append(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            }))
+        }
+    }
+}
+
