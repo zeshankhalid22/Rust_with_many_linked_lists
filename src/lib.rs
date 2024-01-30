c3a632fac8396ec3d7b5d42a1ce1466fd216696b
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2024-01-30 22:09:57 +0500

    now only import files

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,2 @@
-use crate::an_ok_stack::List;
-
 pub mod an_ok_stack;
-mod tests;
-mod persistent_stack;
-
-fn main() {
-    let mut list = List::new();
-    list.push(14);
-    list.push(15);
-    list.push(18);
-
-    let front = list.peek();
-    match front {
-        Some(value) => println!("Top is {}",value),
-        None => println!("empty q."),
-    }
-}
+pub mod persistent_stack;
