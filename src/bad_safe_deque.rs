594f41b94505baa12b3ada28e12ba45de0d556c3
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2024-02-04 20:47:05 +0500

    push and pop (front) functions added

---
+++ src/bad_safe_deque.rs
@@ -0,0 +1,71 @@
+use std::rc::Rc;   // allow multiple owners, but with immutable access
+use std::cell::RefCell;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+// a Node can have multiple owners and can be mutated
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        /// initialize and return an instance of Node<T> wrapped by RefCell, Rc
+        Rc::new(RefCell::new(Node {
+            elem,
+            next: None,
+            prev: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List{head:None, tail:None}
+    }
+    pub fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            // non-empty list
+            Some(old_head) => {
+                // connect old_head with new_head
+                // increase reference count(Rc) of new_head
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+    }
+    pub fn pop_front(&mut self) -> Option<T>{
+        self.head.take().map(|old_head| {
+            // match against next value of old_head
+            match old_head.borrow_mut().next.take() {
+                // non-empty list
+                Some(new_head) => {
+                    // disconnect prev of new_head by replacing it with None
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                }
+                None => {
+                    // if only one element, emptying list
+                    self.head.take();
+                }
+            }
+            // old_head.into_inner().elem // cannot move out of Rc
+            // Rc::try_unwrap moves out the content of Rc if it's RefCount is 1
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+
+        })
+    }
+}
