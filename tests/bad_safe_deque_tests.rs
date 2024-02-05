a15660bd3ae1bb7191033bc322b531323521c235
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2024-02-05 21:21:28 +0500

    pop_back() & push_back() tested

--- tests/bad_safe_deque_tests.rs
+++ tests/bad_safe_deque_tests.rs
@@ -12,22 +12,24 @@ mod test {
         // Populate list
         list.push_front(1);
         list.push_front(2);
-        list.push_front(3);
 
         // Check normal removal
-        assert_eq!(list.pop_front(), Some(3));
         assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(),None);
 
         // Push some more just to make sure nothing's corrupted
-        list.push_front(4);
-        list.push_front(5);
+        list.push_back(4);
+        list.push_back(5);
+        list.push_back(9);
 
-        // Check normal removal
-        assert_eq!(list.pop_front(), Some(5));
-        assert_eq!(list.pop_front(), Some(4));
+        assert_eq!(list.pop_back(),Some(9));
+        assert_eq!(list.pop_back(),Some(5));
+        assert_eq!(list.pop_back(),Some(4));
+        assert_eq!(list.pop_back(),None);
 
-        // Check exhaustion
-        assert_eq!(list.pop_front(), Some(1));
-        assert_eq!(list.pop_front(), None);
+        list.push_back(12);
+        list.push_front(6);
+        drop(list);
     }
 }
